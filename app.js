355302b7294c69f0f4f5031789df87cd371f2d23
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-28 16:35:27 -0600

    Paso 5: Uso del método GET de express para crear un endpoint que regrese una lista - Corregido

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
-// HTTP Methods
+// HTTP Methods GET
+// Endpoint que regresa una lista
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     const explorer1 = {id: 1, name: "Daniela1"}
     const explorer2 = {id: 2, name: "Daniela2"}
-    const explorer3 = {id: 2, name: "Daniela3"}
-    const explorer4 = {id: 2, name: "Daniela4"}
+    const explorer3 = {id: 3, name: "Daniela3"}
+    const explorer4 = {id: 4, name: "Daniela4"}
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     // HTTP CODE STATUS: 200
     res.status(200).json(explorers)
