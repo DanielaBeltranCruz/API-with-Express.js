72756c00775580597b9883cb037ffbb748a68de8
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-28 16:22:22 -0600

    Paso 4: Agregando app de express

--- app.js
+++ app.js
@@ -6,18 +6,6 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
-// HTTP Methods
-app.get('/v1/explorers', (req, res) => {
-    console.log(`Api Explorers GET ALL requests ${new Date()}`)
-    const explorer1 = {id: 1, name: "Daniela1"}
-    const explorer2 = {id: 2, name: "Daniela2"}
-    const explorer3 = {id: 2, name: "Daniela3"}
-    const explorer4 = {id: 2, name: "Daniela4"}
-    const explorers = [explorer1, explorer2, explorer3, explorer4]
-    // HTTP CODE STATUS: 200
-    res.status(200).json(explorers)
-})
-
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
