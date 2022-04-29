557db57af5759952f024348fcff6471f1e44d973
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-28 20:04:55 -0600

    Paso 8: Uso del método PUT para crear un endpoint que se encargue de actualizar los datos creados

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ app.post('/v1/explorers', (req, res) => {
     res.status(201).json({message: "Created"})
 })
 
+// HTTP Methods PUT
+// Endpoint que se encargue de actualizar un explorer
+// localhost:3000/v1/explorers/1
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body // Parámetros de un  cliente
+    res.status(200).json({message: "Updated!"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
